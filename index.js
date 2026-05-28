9614ef49fb34ea9485bbdaced36a930159a58f5a
deepaak0706 <85348791+deepaak0706@users.noreply.github.com>
2026-05-28 21:52:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -629,6 +629,7 @@ app.get("/", (req, res) => {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="manifest" href="/manifest.json">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <title>KK Nagar Weather Station</title>
