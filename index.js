02141b09c747635d04c19c43611df26afaf2e935
deepaak0706 <85348791+deepaak0706@users.noreply.github.com>
2026-08-13 12:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -700,7 +700,7 @@ try {
             atmo: { hum: liveHum, hTrend: humRate, press: livePress, pTrend: pressRate, sol: r.solar, uv: r.uv },
             wind: { speed: liveWind, gust: liveGust, maxS: mx_w, maxSTime: mx_w_t, maxG: mx_g, maxGTime: mx_g_t, deg: r.windDeg, card: getCard(r.windDeg) },
             rain: (() => {
-    let yearlyMm = Math.round((r.yearlyIn * 2540 + (station.id === 'kknagar' ? 494.8 * 100 : station.id === 'ayyapakkam' ? 257.02 * 100 : 0)) / 100);
+    let yearlyMm = Math.round((r.yearlyIn * 2540 + (station.id === 'kknagar' ? 494.9 * 100 : station.id === 'ayyapakkam' ? 257.02 * 100 : 0)) / 100);
 
     return {
         total:   Math.round(r.dailyIn  * 2540) / 100,
