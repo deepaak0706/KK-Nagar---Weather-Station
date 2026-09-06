23cff8a4352a53f3c66202346270c8e3f8bf464e
deepaak0706 <85348791+deepaak0706@users.noreply.github.com>
2026-09-07 00:04:34 +0530

    Add new environment variables for Neelangarai

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const APPLICATION_KEY = process.env.APPLICATION_KEY;
 const API_KEY = process.env.API_KEY;
 const MAC = process.env.MAC;
 
-
 // New — Neelangarai
 const NL_APPLICATION_KEY = process.env.NL_APPLICATION_KEY;
 const NL_API_KEY = process.env.NL_API_KEY;
